84f4cd6fcf08dccb2064da97b152debd107f0b70
Nerys-Thamm <nerysthamm@mgail.com>
2022-04-03 19:04:47 +1200

    STUFF IDK

--- Assets/Scripts/AI/Bosses/Skinwalker/BossSecondState.cs
+++ Assets/Scripts/AI/Bosses/Skinwalker/BossSecondState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossSecondState : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class BossSecondState : MonoBehaviour
             GetComponentInChildren<Animator>().SetBool("Charging", false);
             GetComponent<NodeAI.NodeAI_Agent>().SetState("SecondPhaseTransform");
             GetComponent<AudioSource>().PlayOneShot(transformSoundClip);
+            GetComponent<NavMeshAgent>().isStopped = true;
             
         }
         
