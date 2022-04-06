de3a920b02bf5b96b7852e4b4dff934da0c1a49b
Keane <KeaneCarotenuto@gmail.com>
2022-04-06 14:33:33 +1200

    Player Step Script

---
+++ Assets/PlayerStepSound.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStepSound : MonoBehaviour
+{
+    public List<AudioClip> stepSounds;
+
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayStepSound()
+    {
+        if (_audioSource)
+        {
+            _audioSource.clip = stepSounds[Random.Range(0, stepSounds.Count)];
+            _audioSource.Play();
+        }
+    }
+}
