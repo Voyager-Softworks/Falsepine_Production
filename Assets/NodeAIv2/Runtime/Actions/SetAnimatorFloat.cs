6458c72ae4052831f37f31cdaa5b91799e5ce1f3
Nerys-Thamm <nerysthamm@mgail.com>
2022-05-15 23:40:10 +1200

    Tool stuff

--- Assets/NodeAIv2/Runtime/Actions/SetAnimatorFloat.cs
+++ Assets/NodeAIv2/Runtime/Actions/SetAnimatorFloat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace NodeAI
+namespace NodeAI.Animation
 {
     public class SetAnimatorFloat : ActionBase
     {
