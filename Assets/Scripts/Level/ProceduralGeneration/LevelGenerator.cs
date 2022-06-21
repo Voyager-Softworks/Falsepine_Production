73725bc52777e2cb4385dc69e8c893a0c6787358
Nerys-Thamm <nerysthamm@mgail.com>
2022-06-21 19:36:27 +1200

    Added emersons features to tile spawny thingie

--- Assets/Scripts/Level/ProceduralGeneration/LevelGenerator.cs
+++ Assets/Scripts/Level/ProceduralGeneration/LevelGenerator.cs
@@ -5,7 +5,14 @@ using UnityEngine.AI;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject[] tiles;
+    public Tile[] tiles;
+
+    public struct Tile
+    {
+        public GameObject tile;
+        public float spawnChance;
+        public bool canRotate;
+    }
     public float tileRadius = 1f;
     
     // Start is called before the first frame update
@@ -20,16 +27,44 @@ public class LevelGenerator : MonoBehaviour
         
     }
 
+    Tile[] PickRandomTiles(int count)
+    {
+        List<Tile> pickedTiles = new List<Tile>();
+        for (int i = 0; i < count; i++)
+        {
+            float totalChance = 0;
+            foreach (Tile tile in tiles)
+            {
+                totalChance += tile.spawnChance;
+            }
+            float randomPoint = Random.value * totalChance;
+            float currentChance = 0;
+            foreach (Tile tile in tiles)
+            {
+                currentChance += tile.spawnChance;
+                if (randomPoint <= currentChance)
+                {
+                    pickedTiles.Add(tile);
+                    break;
+                }
+            }
+        }
+        return pickedTiles.ToArray();
+    }
+
     IEnumerator GenerateLevel()
     {
         List<NavMeshSurface> surfaces = new List<NavMeshSurface>();
+        Tile[] pickedTiles = PickRandomTiles(9);
+        int k = 0;
         for(int i = -1; i <= 1; i++)
         {
             for(int j = -1; j <= 1; j++)
             {
                 Vector3 pos = new Vector3(i * (tileRadius * 2.0f), 0, j * (tileRadius * 2.0f));
                 //Instantiate tiles with random rotation
-                surfaces.Add(Instantiate(tiles[Random.Range(0, tiles.Length)], pos, Quaternion.Euler(0, Random.Range(0, 3) * 90, 0)).GetComponent<NavMeshSurface>());
+                surfaces.Add(Instantiate(tiles[k].tile, pos, tiles[k].canRotate ? Quaternion.Euler(0,  Random.Range(0, 3) * 90, 0)  : Quaternion.identity).GetComponent<NavMeshSurface>());
+                k++;
                 yield return new WaitForEndOfFrame();
             }
         }
