9702a39f29677f85348c214b5bc833749bbc2435
Nerys-Thamm <nerysthamm@gmail.com>
2022-08-01 00:36:03 +1200

    doxygen github test

--- Assets/Scripts/TestAction.cs
+++ Assets/Scripts/TestAction.cs
@@ -5,7 +5,7 @@ using NodeAI;
 
 public class TestAction : NodeAI.ActionBase  /// @todo Comment
 {
-    public TestAction() /// @bug Test Bug
+    public TestAction() /// @bug This is a test change to the bug to see if the auto doxygen github thing works
     {
         AddProperty<GameObject>("Target", null);
         AddProperty<Color>("Color", Color.white);
