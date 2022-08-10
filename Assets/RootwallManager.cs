f258969f77db96c914d187793a7354a90b246b63
Nerys-Thamm <nerysthamm@gmail.com>
2022-08-10 21:56:09 +1200

    Wailing tree boss additions

--- Assets/RootwallManager.cs
+++ Assets/RootwallManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 
+/// </summary>
 public class RootwallManager : MonoBehaviour
 {
     public float spawnRadius = 10f;
@@ -9,6 +12,8 @@ public class RootwallManager : MonoBehaviour
     public float maxSpawnAngle = 360f;
 
     public GameObject rootwallPrefab;
+
+    public float duration = 10f;
     List<Vector3> gizmoSpawnPositions;
     List<float> gizmoSpawnAngles;
     Mesh rootwallMesh;
@@ -16,19 +21,40 @@ public class RootwallManager : MonoBehaviour
     public bool generateExampleRootwall = false;
     public int exampleRootwallCount = 10;
 
+    List<GameObject> spawned;
+    List<float> spawnedTimes;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawned = new List<GameObject>();
+        spawnedTimes = new List<float>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < spawned.Count; i++)
+        {
+            if (Time.time - spawnedTimes[i] > duration)
+            {
+                Destroy(spawned[i], 10);
+                spawned[i].GetComponentInChildren<Animator>().SetTrigger("Lower");
+                spawned.RemoveAt(i);
+                spawnedTimes.RemoveAt(i);
+                break;
+            }
+        }
+    }
+
+    public void SpawnRootwall()
+    {
+        GameObject rootwall = Instantiate(rootwallPrefab, GetSpawnPosition(), Quaternion.Euler(0, Random.Range(0, 360), 0));
+        spawned.Add(rootwall);
+        spawnedTimes.Add(Time.time);
     }
 
     void OnDrawGizmosSelected()
@@ -49,6 +75,7 @@ public class RootwallManager : MonoBehaviour
         {
             rootwallMesh = rootwallPrefab.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
         }
+        
         if(generateExampleRootwall)
         {
             gizmoSpawnAngles = new List<float>();
@@ -60,15 +87,19 @@ public class RootwallManager : MonoBehaviour
             }
             generateExampleRootwall = false;
         }
-        Gizmos.color = new Color(8.0f/16.0f, 4.0f/16.0f, 1.0f/16.0f);
-        for (int i = 0; i < gizmoSpawnAngles.Count; i++)
+        if(gizmoSpawnAngles != null)
         {
-            Vector3 pos = gizmoSpawnPositions[i];
-            float angle = gizmoSpawnAngles[i];
-            Vector3 offset = rootwallMesh.bounds.center;
-            offset = Quaternion.Euler(0, angle, 0) * offset;
-            Gizmos.DrawWireMesh(rootwallMesh, pos - offset, Quaternion.Euler(0, angle, 0), rootwallPrefab.transform.localScale);
+            Gizmos.color = new Color(8.0f/16.0f, 4.0f/16.0f, 1.0f/16.0f);
+            for (int i = 0; i < gizmoSpawnAngles.Count; i++)
+            {
+                Vector3 pos = gizmoSpawnPositions[i];
+                float angle = gizmoSpawnAngles[i];
+                Vector3 offset = rootwallMesh.bounds.center;
+                offset = Quaternion.Euler(0, angle, 0) * offset;
+                Gizmos.DrawWireMesh(rootwallMesh, pos - offset, Quaternion.Euler(0, angle, 0), rootwallPrefab.transform.localScale);
+            }
         }
+        
         
         
         
