e7c66fd6fd6f4bffcf941f89dc35dc23ebc50257
Keane <KeaneCarotenuto@gmail.com>
2022-08-14 21:49:44 +1200

    revert nerys change

--- Assets/Scripts/TouchTrigger.cs
+++ Assets/Scripts/TouchTrigger.cs
@@ -10,10 +10,6 @@ public class TouchTrigger : MonoBehaviour  /// @todo Comment
     bool triggered = false;
 
     public bool ignorePlayer = false;
-    public bool alwaysCheck = false;
-    public string targetTag = "";
-
-    Collider[] hits = new Collider[10];
 
     // Start is called before the first frame update
     void Start()
@@ -24,24 +20,7 @@ public class TouchTrigger : MonoBehaviour  /// @todo Comment
     // Update is called once per frame
     void Update()
     {
-        if(alwaysCheck && !triggered)
-        {
-            if(Physics.OverlapSphereNonAlloc(transform.position, 1f, hits) > 0)
-            {
-                if(hits.Length == 0 || hits[0] == null) return;
-                for(int i = 0; i < hits.Length; i++)
-                {
-                    if(hits[i].gameObject.tag == targetTag)
-                    {
-                        if(!triggered)
-                        {
-                            triggered = true;
-                            onTrigger.Invoke();
-                        }
-                    }
-                }
-            }
-        }
+        
     }
 
     void OnTriggerEnter(Collider other)
