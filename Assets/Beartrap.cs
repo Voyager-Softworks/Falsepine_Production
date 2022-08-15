58aacc776c21f8d91365536ade02f8ab44a7bf6d
Keane <KeaneCarotenuto@gmail.com>
2022-08-15 22:52:07 +1200

    Bear trap does damage and ignores player/beartraps

--- Assets/Beartrap.cs
+++ Assets/Beartrap.cs
@@ -5,6 +5,8 @@ using System.Linq;
 
 public class Beartrap : MonoBehaviour
 {
+    public float m_damage = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,17 @@ public class Beartrap : MonoBehaviour
     {
         gameObject.transform.root.gameObject.GetComponent<Animator>().SetTrigger("Close");
         gameObject.transform.root.gameObject.GetComponentsInChildren<Collider>().All(collider => collider.enabled = false);
-    }
 
-    
+        Collider hitCollider = GetComponent<TouchTrigger>().hitCollider;
+        if (hitCollider != null)
+        {
+            // try deal damage to the object
+            Health_Base health = hitCollider.GetComponentInParent<Health_Base>();
+            if (health == null) health = hitCollider.GetComponentInChildren<Health_Base>();
+            if (health != null)
+            {
+                health.TakeDamage(new Health_Base.DamageStat(m_damage, gameObject, transform.position, hitCollider.transform.position));
+            }
+        }
+    }
 }
