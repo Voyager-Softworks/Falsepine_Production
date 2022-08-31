fc1435750b2365e625928ef97a6b04777bbef026
Keane <KeaneCarotenuto@gmail.com>
2022-09-01 02:06:24 +1200

    Made the town camera drag sexier
    For real, its so nice now

--- Assets/Scripts/Level/CameraDrag.cs
+++ Assets/Scripts/Level/CameraDrag.cs
@@ -13,12 +13,15 @@ public class CameraDrag : MonoBehaviour
     // horiz and vert input axes for the camera to be moved using keys
     public InputAction m_horizInput;
     public InputAction m_vertInput;
+    public InputAction m_boostInput;
 
     public float m_keySpeed = 2.0f;
+    public float m_boostMulti = 2.0f;
+    public Vector3 m_velocity = Vector3.zero;
 
 
     public float dragSpeed = 2;
-    private Vector2 dragOrigin;
+    private Vector3 dragOrigin;
 
     public Vector3 center = new Vector3(0, 0, 0);
     public float maxDistance = 10;
@@ -33,11 +36,13 @@ public class CameraDrag : MonoBehaviour
     private void OnEnable() {
         m_horizInput.Enable();
         m_vertInput.Enable();
+        m_boostInput.Enable();
     }
 
     private void OnDisable() {
         m_horizInput.Disable();
         m_vertInput.Disable();
+        m_boostInput.Disable();
     }
  
  
@@ -54,7 +59,7 @@ public class CameraDrag : MonoBehaviour
         CheckDrag();
 
         //lerp to update position
-        transform.position = Vector3.Lerp(transform.position, m_targetPosition, Time.deltaTime * 50.0f);
+        transform.position = m_targetPosition;
     }
 
     /// <summary>
@@ -62,20 +67,46 @@ public class CameraDrag : MonoBehaviour
     /// </summary>
     private void CheckDrag()
     {
+        Vector3 currentMousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(currentMousePos);
+
         if (Mouse.current.rightButton.wasPressedThisFrame)
         {
-            dragOrigin = Mouse.current.position.ReadValue();
+            // raycast into the world
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // set the drag origin to the hit point
+                dragOrigin = hit.point;
+            }
             return;
         }
 
         if (!Mouse.current.rightButton.isPressed) return;
 
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Mouse.current.delta.ReadValue());
-        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-        //rotate move arounjd the y axis
-        move = Quaternion.Euler(0, -45, 0) * -move;
+        // make a plane at dragOrigin, with normal pointing up
+        Plane plane = new Plane(Vector3.up, dragOrigin);
 
-        m_targetPosition = transform.position + move;
+        // get the mouse position on the plane
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 point = ray.GetPoint(distance);
+
+            // get the difference between the mouse position and the drag origin
+            Vector3 difference = dragOrigin - point;
+            difference.y = 0;
+            // set the target position to the difference
+            m_targetPosition = Vector3.Lerp(m_targetPosition, transform.position + difference, Time.deltaTime * 20.0f);
+        }
+
+
+        // Vector3 pos = Camera.main.ScreenToViewportPoint(Mouse.current.delta.ReadValue());
+        // Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+        // //rotate move arounjd the y axis
+        // move = Quaternion.Euler(0, -45, 0) * -move;
+
+        // m_targetPosition = transform.position + move;
     }
 
     /// <summary>
@@ -85,6 +116,7 @@ public class CameraDrag : MonoBehaviour
     {
         Vector3 xMove = Vector3.zero;
         Vector3 zMove = Vector3.zero;
+        float multi = 1;
 
         if (m_horizInput.ReadValue<float>() != 0)
         {
@@ -96,10 +128,15 @@ public class CameraDrag : MonoBehaviour
             zMove = transform.forward * m_vertInput.ReadValue<float>() * m_keySpeed * Time.deltaTime;
             zMove.y = 0;
         }
+        if (m_boostInput.ReadValue<float>() != 0)
+        {
+            multi = m_boostMulti;
+        }
 
-        Vector3 move = (xMove + zMove).normalized * Mathf.Max(xMove.magnitude, zMove.magnitude);
+        Vector3 move = (xMove + zMove).normalized * Mathf.Max(xMove.magnitude, zMove.magnitude) * multi;
+        m_velocity = Vector3.Lerp(m_velocity, move, Time.deltaTime * 10.0f);
         
-        m_targetPosition = transform.position + move;
+        m_targetPosition = transform.position + m_velocity;
     }
 
 }
