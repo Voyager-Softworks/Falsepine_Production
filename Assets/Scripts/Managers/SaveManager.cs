409411feee316a20884d5f10081c3944f7f68f9b
Keane <KeaneCarotenuto@gmail.com>
2022-09-11 17:55:29 +1200

    Build friendly folder copy

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -213,7 +213,7 @@ public class SaveManager : MonoBehaviour
 
             // copy the folders (including subfolders)
             //@todo use a build friendly way to copy folders
-            FileUtil.CopyFileOrDirectory(folder, newFolder);
+            Copy(folder, newFolder);
         }
 
         SoftDeleteAll(_saveSlot);
@@ -230,6 +230,44 @@ public class SaveManager : MonoBehaviour
             File.Delete(metaFile);
         }
         #endif
+    }
+
+    /// <summary>
+    /// Copies all folders and files (including subfolders) from the sourceDirectory to the targetDirectory.
+    /// </summary>
+    /// <param name="sourceDirectory"></param>
+    /// <param name="targetDirectory"></param>
+    public static void Copy(string sourceDirectory, string targetDirectory)
+    {
+        DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+        DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+        CopyAll(diSource, diTarget);
+    }
+
+    /// <summary>
+    /// Copies all contents of source to target, intended to be used with the Copy function
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="target"></param>
+    private static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+        Directory.CreateDirectory(target.FullName);
+
+        // Copy each file into the new directory.
+        foreach (FileInfo fi in source.GetFiles())
+        {
+            //Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+        }
+
+        // Copy each subdirectory using recursion.
+        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+        {
+            DirectoryInfo nextTargetSubDir =
+                target.CreateSubdirectory(diSourceSubDir.Name);
+            CopyAll(diSourceSubDir, nextTargetSubDir);
+        }
     }
 
     /// <summary>
