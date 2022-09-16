39b1173a241cab9e8714dcbf7dd554898b1894fb
Keane <KeaneCarotenuto@gmail.com>
2022-09-17 04:26:56 +1200

    Speedrun can have Trigger monster

--- Assets/Scripts/Missions/MissionConditions/Speedrun_MissionCondition.cs
+++ Assets/Scripts/Missions/MissionConditions/Speedrun_MissionCondition.cs
@@ -11,11 +11,19 @@ using System;
 public class Speedrun_MissionCondition : MissionCondition
 {
     public float m_timeLimit = 60f;
-    private float m_timeElapsed = 0f;
+    [SerializeField, HideInInspector] private float m_timeElapsed = 0f;
     private bool m_updateTimer = false;
 
+    [Header("Optional")]
+    [Tooltip("If monster specified, timer will only start when enemy is first scene")]public MonsterInfo m_triggerMonster = null;
+    [SerializeField, HideInInspector] private bool m_triggerMonsterSeen = true;
+
     public override string GetDescription(){
-        return "Spend less than " + (m_timeLimit - m_timeElapsed).ToString("0") + " seconds in the expedition";
+        string desc = "Spend less than " + (m_timeLimit - m_timeElapsed).ToString("0") + " seconds in the expedition";
+        if (m_triggerMonster != null){
+            desc += " while fighting " + (m_triggerMonster.m_type == MonsterInfo.MonsterType.Boss ? "the " : "a ") + m_triggerMonster.m_name;
+        }
+        return desc;
     }
 
     public override void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
@@ -24,10 +32,28 @@ public class Speedrun_MissionCondition : MissionCondition
 
         // only update the time if this scene is one of the zone scenes
         m_updateTimer = false;
-        foreach (Utilities.SceneField scene in MissionManager.instance.GetCurrentZone().GetSceneList()){
-            if (arg0.path.Contains(scene.scenePath)){
+        foreach (Utilities.SceneField scene in MissionManager.instance.GetCurrentZone().GetSceneList())
+        {
+            if (arg0.path.Contains(scene.scenePath))
+            {
                 m_updateTimer = true;
                 break;
+            }
+        }
+
+        // find all Health_Base objects and check if they are the trigger monster
+        if (m_updateTimer && m_triggerMonster != null)
+        {
+            m_triggerMonsterSeen = false;
+            Health_Base[] healths = GameObject.FindObjectsOfType<Health_Base>();
+            foreach (Health_Base health in healths)
+            {
+                EnemyHealth enemyHealth = health as EnemyHealth;
+                if (enemyHealth != null && enemyHealth.m_monsterType == m_triggerMonster)
+                {
+                    m_triggerMonsterSeen = true;
+                    break;
+                }
             }
         }
     }
@@ -45,9 +71,11 @@ public class Speedrun_MissionCondition : MissionCondition
         base.Update();
 
         // update timer
-        if (m_updateTimer && !m_lockState){
+        if (m_updateTimer && m_triggerMonsterSeen && !m_lockState)
+        {
             m_timeElapsed += Time.deltaTime;
-            if (m_timeElapsed > m_timeLimit){
+            if (m_timeElapsed >= m_timeLimit)
+            {
                 SetState(ConditionState.FAILED);
             }
         }
