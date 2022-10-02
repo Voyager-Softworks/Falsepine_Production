cd012c55cbdd351e2f18d551bc63c46fcc10e090
Nerys-Thamm <nerysthamm@gmail.com>
2022-10-02 20:00:23 +1300

    Lightning changes

---
+++ Assets/Scripts/Enemies/Bosses/Omen/Nodes/Raise.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeAI;
+
+namespace Boss.Omen
+{
+    public class Raise : NodeAI.ActionBase
+    {
+
+    }
+}
